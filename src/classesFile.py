877b669208c77a61b03e7467332ab5ffc040c341
Ahmed Mohammed Wahba <67748256+abowahbaz@users.noreply.github.com>
2026-05-03 01:40:34 +0300

    Refactor manage connections

---
+++ src/classesFile.py
@@ -0,0 +1,8 @@
+from manage_books import ManageBook
+from manage_users import ManageUsers
+from manage_bill import ManageBill
+from databasesFile import main_database_conection
+
+manage_book_conection = ManageBook(main_database_conection)
+manage_user_conection = ManageUsers(main_database_conection)
+manage_bill_conection = ManageBill(main_database_conection)
